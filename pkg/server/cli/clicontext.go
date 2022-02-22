391bd55bb0a985b2d1a7e7b2ac0706119c39ef5b
Phillip Rak <rak.phillip@gmail.com>
2022-02-22 13:40:57 -0700

    Assign the offline flag if it contains true/false/dynamic
    Otherwise, default to dynamic to keep behavior the same as it is today
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/server/cli/clicontext.go
+++ pkg/server/cli/clicontext.go
@@ -53,7 +53,7 @@ func (c *Config) ToServer(ctx context.Context, sqlCache bool) (*server.Server, e
 
 	return server.New(ctx, restConfig, &server.Options{
 		AuthMiddleware: auth,
-		Next:           ui.New(c.UIPath),
+		Next:           ui.New(c.UIPath, c.Offline),
 		SQLCache:       sqlCache,
 		SQLCacheFactoryOptions: factory.CacheFactoryOptions{
 			GCInterval:  15 * time.Minute,
