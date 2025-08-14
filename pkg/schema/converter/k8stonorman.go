rancher/steve

Reducing number of generated schemas
Changes steve to only return top level schema definitions rather
than a full defined schema for every field. Also adds basic docs
and fixes a bug with schema generation on versioned crds

pkg/schema/converter/k8stonorman.go
@@ -1,3 +1,5 @@
+// Package converter is responsible for converting the types registered with a k8s server to schemas which can be used
+// by the UI (and other consumers) to discover the resources available and the current user's permissions.
 package converter
 
 import (
@@ -5,9 +7,18 @@ import (
 	"strings"
 
 	"github.com/rancher/apiserver/pkg/types"
+	"github.com/rancher/norman/types/convert"
 	v1 "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
+	"k8s.io/kube-openapi/pkg/util/proto"
+)
+
+const (
+	gvkExtensionName    = "x-kubernetes-group-version-kind"
+	gvkExtensionGroup   = "group"
+	gvkExtensionVersion = "version"
+	gvkExtensionKind    = "kind"
 )
 
 func GVKToVersionedSchemaID(gvk schema.GroupVersionKind) string {
@@ -38,18 +49,40 @@ func GVRToPluralName(gvr schema.GroupVersionResource) string {
 	return fmt.Sprintf("%s.%s", gvr.Group, gvr.Resource)
 }
 
+// GetGVKForKind attempts to retrieve a GVK for a given Kind. Not all kind represent top level resources,
+// so this function may return nil if the kind did not have a gvk extension
+func GetGVKForKind(kind *proto.Kind) *schema.GroupVersionKind {
+	extensions, ok := kind.Extensions[gvkExtensionName].([]any)
+	if !ok {
+		return nil
+	}
+	for _, extension := range extensions {
+		if gvkExtension, ok := extension.(map[any]any); ok {
+			gvk := schema.GroupVersionKind{
+				Group:   convert.ToString(gvkExtension[gvkExtensionGroup]),
+				Version: convert.ToString(gvkExtension[gvkExtensionVersion]),
+				Kind:    convert.ToString(gvkExtension[gvkExtensionKind]),
+			}
+			return &gvk
+		}
+	}
+	return nil
+}
+
+// ToSchemas creates the schemas for a K8s server, using client to discover groups/resources, and crd to potentially
+// add additional information about new fields/resources. Mostly ties together AddDiscovery and AddCustomResources.
 func ToSchemas(crd v1.CustomResourceDefinitionClient, client discovery.DiscoveryInterface) (map[string]*types.APISchema, error) {
 	result := map[string]*types.APISchema{}
-
-	if err := AddOpenAPI(client, result); err != nil {
-		return nil, err
-	}
 
 	if err := AddDiscovery(client, result); err != nil {
 		return nil, err
 	}
 
 	if err := AddCustomResources(crd, result); err != nil {
+		return nil, err
+	}
+
+	if err := AddDescription(client, result); err != nil {
 		return nil, err
 	}
 

rancher-sandbox/rancher-desktop-steve

pkg/schema/converter/k8stonorman.go
@@ -1,3 +1,5 @@
+// Package converter is responsible for converting the types registered with a k8s server to schemas which can be used
+// by the UI (and other consumers) to discover the resources available and the current user's permissions.
 package converter
 
 import (
@@ -5,9 +7,18 @@ import (
 	"strings"
 
 	"github.com/rancher/apiserver/pkg/types"
+	"github.com/rancher/norman/types/convert"
 	v1 "github.com/rancher/wrangler/v2/pkg/generated/controllers/apiextensions.k8s.io/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
+	"k8s.io/kube-openapi/pkg/util/proto"
+)
+
+const (
+	gvkExtensionName    = "x-kubernetes-group-version-kind"
+	gvkExtensionGroup   = "group"
+	gvkExtensionVersion = "version"
+	gvkExtensionKind    = "kind"
 )
 
 func GVKToVersionedSchemaID(gvk schema.GroupVersionKind) string {
@@ -38,18 +49,40 @@ func GVRToPluralName(gvr schema.GroupVersionResource) string {
 	return fmt.Sprintf("%s.%s", gvr.Group, gvr.Resource)
 }
 
+// GetGVKForKind attempts to retrieve a GVK for a given Kind. Not all kind represent top level resources,
+// so this function may return nil if the kind did not have a gvk extension
+func GetGVKForKind(kind *proto.Kind) *schema.GroupVersionKind {
+	extensions, ok := kind.Extensions[gvkExtensionName].([]any)
+	if !ok {
+		return nil
+	}
+	for _, extension := range extensions {
+		if gvkExtension, ok := extension.(map[any]any); ok {
+			gvk := schema.GroupVersionKind{
+				Group:   convert.ToString(gvkExtension[gvkExtensionGroup]),
+				Version: convert.ToString(gvkExtension[gvkExtensionVersion]),
+				Kind:    convert.ToString(gvkExtension[gvkExtensionKind]),
+			}
+			return &gvk
+		}
+	}
+	return nil
+}
+
+// ToSchemas creates the schemas for a K8s server, using client to discover groups/resources, and crd to potentially
+// add additional information about new fields/resources. Mostly ties together AddDiscovery and AddCustomResources.
 func ToSchemas(crd v1.CustomResourceDefinitionClient, client discovery.DiscoveryInterface) (map[string]*types.APISchema, error) {
 	result := map[string]*types.APISchema{}
 
-	if err := AddOpenAPI(client, result); err != nil {
+	if err := addDiscovery(client, result); err != nil {
 		return nil, err
 	}
 
-	if err := AddDiscovery(client, result); err != nil {
+	if err := addCustomResources(crd, result); err != nil {
 		return nil, err
 	}
 
-	if err := AddCustomResources(crd, result); err != nil {
+	if err := addDescription(client, result); err != nil {
 		return nil, err
 	}
 